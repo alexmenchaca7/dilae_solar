4846facde2228bc7619dd6aee709c7e6b61b5e8a
Alejandro Menchaca <alexmenchaca7@gmail.com>
2025-08-22 19:55:15 -0600

    feat: agregar imagenes fijas en todos los heros de la pagina y cambiar el llamado a la accion de la pagina de soluciones (integrando una nueva imagen de fondo y cambiando textos)

--- views/templates/hero-index.php
+++ views/templates/hero-index.php
@@ -1,9 +1,12 @@
 <section class="hero hero-index">
     <div class="contenido-header">
         <div class="contenedor">
-            <h1>Deje de pagarle a CFE. Invierta en el activo más importante: <span>su hogar.</span></h1>
-            <p>Instalación de paneles solares en Guadalajara que se pagan solos y aumentan la plusvalía de su propiedad.</p>
-            <a href="/contacto" class="btn-contacto">Solicitar mi Estudio de Ahorro</a>
+            <h1>Su camino hacia la energía solar empieza aquí</h1>
+            <p>Si busca ahorro, confianza y expertos en instalación de paneles solares en Guadalajara, está en el lugar correcto.</p>
+            <div class="botones">
+                <a href="/calculadora" class="btn-calculadora-index">Calcule su Ahorro</a>
+                <a href="/contacto" class="btn-contacto-index">Obtenga su Cotización</a>
+            </div>
         </div>
     </div>
 </section>
