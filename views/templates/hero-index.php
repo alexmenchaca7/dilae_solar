ea345501c969e324fc9735d771eb147af7e6d7f5
Alejandro Menchaca <alexmenchaca7@gmail.com>
2025-09-19 21:25:46 -0600

    feat: agregar titulo h1 en la pagina de inicio

--- views/templates/hero-index.php
+++ views/templates/hero-index.php
@@ -1,7 +1,8 @@
 <section class="hero hero-index">
     <div class="contenido-header">
         <div class="contenedor">
-            <h1>Su camino hacia la energía solar empieza aquí</h1>
+            <h1>Paneles Solares Guadalajara</h1>
+            <h2>Su camino hacia la energía solar empieza aquí</h2>
             <p>Si busca ahorro, confianza y expertos en instalación de paneles solares en Guadalajara, está en el lugar correcto.</p>
             <div class="botones">
                 <a href="/calculadora" class="btn-calculadora-index">Calcule su Ahorro</a>
